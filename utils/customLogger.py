cb4a1ab24fa1060d96bd5a8194b3d9c2db0f3bae
Rudra Pratap Singh <>
2024-10-07 15:13:27 +0200

    updated logger

--- utils/customLogger.py
+++ utils/customLogger.py
@@ -1,11 +1,21 @@
 import logging
+import os
+
 
 class logGen:
     @staticmethod
     def loggen():
+        log_dir = "./Logs/"
+        if not os.path.exists(log_dir):
+            os.makedirs(log_dir)
+
         logging.basicConfig(
-            filename="./Logs/automation.log",
-            format = '%(asctime)s -%(name)s -%(levelname)s - %(message)s',
-            datefmt = '%m/%d/%Y %I:%M:%S', level = logging.INFO)
+            filename=os.path.join(log_dir, "automation.log"),
+            format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+            datefmt='%m/%d/%Y %I:%M:%S %p',
+            level=logging.INFO,
+            force=True
+        )
+
         logger = logging.getLogger()
         return logger
